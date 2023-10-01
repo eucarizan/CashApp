893a7202cbea75f6219d2cf959965cac1754d104
eucarizan <eucarizan@gmail.com>
2023-10-01 15:33:16 +0800

    repositories & spring data

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class CashcardApplicationTests {
+class CashCardApplicationTests {
 
 	@Autowired
 	TestRestTemplate restTemplate;
