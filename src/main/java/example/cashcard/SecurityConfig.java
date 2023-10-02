a430addfd174fadb719159c19cd748a54c3012bb
eucarizan <eucarizan@gmail.com>
2023-10-02 23:28:05 +0800

    Simple Spring Security: Added the Spring Security Dependency, Added SecurityConfig.java

---
+++ src/main/java/example/cashcard/SecurityConfig.java
@@ -0,0 +1,18 @@
+package example.cashcard;
+
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+}
