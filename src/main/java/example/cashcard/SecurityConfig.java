42df82b54277f53ddfb2c21c8b851ee9fca85948
eucarizan <eucarizan@gmail.com>
2023-10-02 23:38:40 +0800

    Simple Spring Security: Configure Basic Authentication

--- src/main/java/example/cashcard/SecurityConfig.java
+++ src/main/java/example/cashcard/SecurityConfig.java
@@ -2,7 +2,9 @@ package example.cashcard;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,7 +14,19 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.build();
+//        http.authorizeHttpRequests()
+//                .requestMatchers("/cashcards/**")
+//                .authenticated()
+//                .and().csrf().disable().httpBasic();
+//
+//        return http.build();
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/cashcards/**").authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 
     @Bean
