14e0143f8cd240b1d7d4f1600adf69809c0f96b5
eucarizan <eucarizan@gmail.com>
2023-10-03 15:38:19 +0800

    Implementing DELETE: Implement the DELETE Endpoint

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -80,6 +80,7 @@ public class CashCardController {
 
     @DeleteMapping("/{id}")
     private ResponseEntity<Void> deleteCashCard(@PathVariable Long id) {
+        cashCardRepository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
